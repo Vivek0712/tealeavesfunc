351d671ff6e48b7fd5352ea6b20434693a4a3519
Vivek Raja P S <vivekraja98@gmail.com>
2021-10-31 14:50:06 +0530

    inference code added

--- predict/__init__.py
+++ predict/__init__.py
@@ -28,6 +28,20 @@ def main(req: func.HttpRequest,context: func.Context) -> func.HttpResponse:
    
 
     # Inference
+    img = np.asarray(image)
+    img = letterbox(img, new_shape=640)[0]
+
+        # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+    # img = np.array(image.getdata()).reshape(image.size[0], image.size[1], 3)
+    img = torch.from_numpy(img).to('cpu')
+    img =  img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    pred = model(img, augment=False)[0]
+
 
  
  
